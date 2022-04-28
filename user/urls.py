8100ba701a5d85e0833a04cfd179d8c63a87ac6f
eliashhtores <eliashhtorres@gmail.com>
2022-04-28 18:04:18 -0500

    WIP

--- user/urls.py
+++ user/urls.py
@@ -1,2 +1,10 @@
-from django.contrib import admin
 from django.urls import path
+from .views import CreateUserView, CreateTokenView, ManageUserView
+
+app_name = 'user'
+
+urlpatterns = [
+    path('login/', CreateTokenView.as_view(), name='login'),
+    path('create/', CreateUserView.as_view(), name='create'),
+    path('me/', ManageUserView.as_view(), name='me'),
+]
